7ed9be8fbac2d266707e22698ed8518cde57447e
Kothai_S <102270819+JebaMercy14@users.noreply.github.com>
2025-07-05 12:43:28 +0530

    Update finalGPA.cs
    Assignment, Print and Escape Sequences

--- finalGPA.cs
+++ finalGPA.cs
@@ -1,7 +1,7 @@
 string studentName = "Sophia Johnson";
 string course1Name = "English 101";
 string course2Name = "Algebra 101";
-string course3Name = "Biology 101";
+string course3Name = "Biology 101"; 
 string course4Name = "Computer Science I";
 string course5Name = "Psychology 101";
 
